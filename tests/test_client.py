bd5041c0e9d1adb6bb9c5856ea5b46488bd25e4c
realoksi <15335458+realoksi@users.noreply.github.com>
2024-04-21 17:43:04 -0400

    warning comment and test_current_user_guilds

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,10 @@
-import datetime
 import unittest
 
 import discord_client
+
+# WARNING
+## For these tests to work, a proper Discord token is required.
+## Without one, these will fail or at the very least behave in an unexpected manner.
 
 
 class test_client(unittest.TestCase):
@@ -9,7 +12,12 @@ class test_client(unittest.TestCase):
         self.client = discord_client.Client("faketoken")
 
     def test_current_user_guilds(self):
-        pass
+        current_user_guilds = self.client.get_current_user_guilds()
+
+        if not current_user_guilds:
+            print("User is not in any guilds?")
+        else:
+            print(len(current_user_guilds))
 
     def test_discoverable_guilds(self):
         discoverable_guilds = self.client.get_discoverable_guilds()
